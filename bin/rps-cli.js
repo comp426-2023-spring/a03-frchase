aad449ab897b0755f1137d0312a54e8dd4df2a09
frchase <69663529+frchase@users.noreply.github.com>
2023-05-09 01:45:40 -0400

    added necessary vars

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -9,14 +9,14 @@ const help = `
     Usage: node-rps [SHOT]
     Play Rock Paper Scissors (RPS)
 
-    -h, --help      display this help message and exit
-    -r, --rules     display the rules and exit
+        -h, --help      display this help message and exit
+        -r, --rules     display the rules and exit
 
     Examples:
-    node-rps        Return JSON with single player RPS result.
-                    e.g. {"player":"rock"}
-    node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                    e.g {"player":"rock","opponent":"scissors","result":"win"}
+        node-rps        Return JSON with single player RPS result.
+                        e.g. {"player":"rock"}
+        node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                        e.g {"player":"rock","opponent":"scissors","result":"win"}
 `;
 
 const rules = `
